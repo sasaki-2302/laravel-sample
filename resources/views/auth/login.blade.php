edae8bfb2068b1188fbeadb700452e4edfbdf70f
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-06-21 15:19:11 +0900

    add_recapcha

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,6 +51,12 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                {!! Captcha::display() !!}
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
